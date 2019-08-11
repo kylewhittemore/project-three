1c5bf5d3bbed5c829adb572ad94725b06f00e0da
KDW <kyle.whittemore@gmail.com>
2019-08-11 12:26:48 -0400

    added MVC-style file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       <p>this is an App</p>
-    </div>
+    </>
   );
 }
 
