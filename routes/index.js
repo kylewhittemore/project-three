d63959352bb9664e39f1d57c19ff2579dd5c6c61
KDW <kyle.whittemore@gmail.com>
2019-08-17 10:47:32 -0400

    add dependencies, edit boilerplate file structure

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const routes = require('express').Router();
 
+// this is the default /api path which requires the 'routes/api' directory where it references the index.js file for the remaining '/api/....' routes
+// I have orgainized it so that each route has a file for maintainability.  To view all of the '/api' routes as a list with their test statuses use the '/routes/api/index.js' file
 routes.get("/api", function (req, res) {
     res.status(200).json({ message: "connected!" });
 });
