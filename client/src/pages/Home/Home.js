8f81790be9bbac85053dacbeebf04618e6a4c5aa
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-18 19:36:41 -0400

    forgot return function and div; camlecased homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,36 +5,38 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import "./style.css";
 
-function homePage() {
-
-    <Container>
-        <Row className="justify-content-md-center">
-            <Button variant="outline" size="lg">
-                <Link to="/newseason">New Season</Link>
-            </Button>
-        </Row>
-        <Row className="justify-content-md-center">
-            <Button variant="outline" size="lg">
-                <Link to="/currentseason:id">Current Seasons</Link>
-            </Button>
-        </Row>
-        <Row className="justify-content-md-center">
-            <Button variant="outline" size="lg">
-                <Link to="/pastseasons">Past Seasons</Link>
-            </Button>
-        </Row>
-        <Row className="justify-content-md-center">
-            <Button variant="outline" size="lg">
-                <Link to="/settings">Settings</Link>
-            </Button>
-        </Row>
-        <Row className="justify-content-md-center">
-            <Button variant="outline" size="lg">
-                <Link to="/logout">Log Out</Link>
-            </Button>
-        </Row>
-    </Container>
-
+function HomePage() {
+    return (
+        <div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Button variant="outline" size="lg">
+                        <Link to="/newseason">New Season</Link>
+                    </Button>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Button variant="outline" size="lg">
+                        <Link to="/currentseason:id">Current Seasons</Link>
+                    </Button>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Button variant="outline" size="lg">
+                        <Link to="/pastseasons">Past Seasons</Link>
+                    </Button>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Button variant="outline" size="lg">
+                        <Link to="/settings">Settings</Link>
+                    </Button>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Button variant="outline" size="lg">
+                        <Link to="/logout">Log Out</Link>
+                    </Button>
+                </Row>
+            </Container>
+        </div>
+    )
 };
 
-export default homePage();
+export default HomePage();
