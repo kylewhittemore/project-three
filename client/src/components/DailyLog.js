339d76b532385cec002e499af575ebc451f4ff90
KDW <kyle.whittemore@gmail.com>
2019-08-18 09:32:59 -0400

    form data controlling form state

--- client/src/components/DailyLog.js
+++ client/src/components/DailyLog.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col';
 
 export default function DailyLog(props) {
+
+    const initialFormState = {
+        date: "",
+        plantAppearance: "",
+        didWater: false,
+        didFeed: false,
+        didTransplant: false,
+        notes: ""
+    }
+
+    const [formData, setFormData] = useState(initialFormState)
+
+    const handleInputChange = event => {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        
+        setFormData({ ...formData, [name]: value })
+        console.log(formData)
+    }
 
     return (
 
@@ -12,13 +32,13 @@ export default function DailyLog(props) {
                 <Col>
                     <Form.Group className="m-1" controlId="log.ControlInput1">
                         <Form.Label>Date:</Form.Label>
-                        <Form.Control type="date" placeholder="name@example.com" />
+                        <Form.Control value={formData.date} name="date" onChange={handleInputChange} type="date" />
                     </Form.Group>
                 </Col>
                 <Col>
-                    <Form.Group className="m-1" controlId="exampleForm.ControlSelect1">
+                    <Form.Group className="m-1" controlId="log.ControlSelect1">
                         <Form.Label>Plant appearance:</Form.Label>
-                        <Form.Control as="select">
+                        <Form.Control value={formData.plantAppearance} name="plantAppearance" onChange={handleInputChange} as="select">
                             <option>Happy</option>
                             <option>Neutral</option>
                             <option>Sad</option>
@@ -29,24 +49,24 @@ export default function DailyLog(props) {
             <Form.Row className="ml-2 m-1">
                 <Col>
                     <Form.Group className="m-1" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Water" />
+                        <Form.Check value={formData.didWater} name="didWater" onChange={handleInputChange} type="checkbox" label="Water" />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group className="m-1" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Feed" />
+                        <Form.Check value={formData.didFeed} name="didFeed" onChange={handleInputChange} type="checkbox" label="Feed" />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group className="m-1" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Transplant" />
+                        <Form.Check value={formData.didTransplant} name="didTransplant" onChange={handleInputChange} type="checkbox" label="Transplant" />
                     </Form.Group>
                 </Col>
             </Form.Row>
             <Col>
                 <Form.Group className="m-1" controlId="log.ControlTextarea1">
                     <Form.Label>Notes:</Form.Label>
-                    <Form.Control as="textarea" rows="3" />
+                    <Form.Control value={formData.notes} name="notes" onChange={handleInputChange} as="textarea" rows="3" />
                 </Form.Group>
             </Col>
             <Button className="m-2" variant="primary" type="submit">
