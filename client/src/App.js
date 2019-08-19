34950a2acad4892a0546c7139abf983579d6a667
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-19 19:14:02 -0400

    imported bootstrap to home.js; centered links on page and routing to correct URL in address bar; need vertical and horizontal centering and overall style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from "./pages/Home/Home";
 
 function App() {
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,33 +4,35 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import "./Home.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function HomePage() {
     return (
-        <div>
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Button variant="outline" size="lg">
+        <div className="align-me">
+            <Container className="page-center">
+                <Row className="justify-content-center">
+                    <Button variant="outline-success" size="lg">
                         <Link to="/newseason">New Season</Link>
                     </Button>
                 </Row>
-                <Row className="justify-content-md-center">
-                    <Button variant="outline" size="lg">
+                <Row className="justify-content-center">
+                    <Button variant="outline-success" size="lg">
                         <Link to="/currentseason:id">Current Seasons</Link>
                     </Button>
                 </Row>
-                <Row className="justify-content-md-center">
-                    <Button variant="outline" size="lg">
+                <Row className="justify-content-center">
+                    <Button variant="outline-success" size="lg">
                         <Link to="/pastseasons">Past Seasons</Link>
                     </Button>
                 </Row>
-                <Row className="justify-content-md-center">
-                    <Button variant="outline" size="lg">
+                <Row className="justify-content-center">
+                    <Button variant="outline-success" size="lg">
                         <Link to="/settings">Settings</Link>
                     </Button>
                 </Row>
-                <Row className="justify-content-md-center">
-                    <Button variant="outline" size="lg">
+                <Row className="justify-content-center">
+                    <Button variant="outline-success" size="lg">
                         <Link to="/logout">Log Out</Link>
                     </Button>
                 </Row>
