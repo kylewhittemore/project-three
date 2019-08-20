a38099d5eba92ef06fd6d528a738fc9c1e352884
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-08-20 16:30:22 -0400

    didn't save them ...

--- passport/LocalStrategy.js
+++ passport/LocalStrategy.js
@@ -1,11 +1,12 @@
-const User = require('../database/models/user')
-const LocalStrategy = require('passport-local').Strategy
+const User = require('../database/models/user');
+const LocalStrategy = require('passport-local').Strategy;
+// const bcrypt = require('bcryptjs');
 
 const strategy = new LocalStrategy(
 	{
 		usernameField: 'username' // not necessary, DEFAULT
 	},
-	function(username, password, done) {
+	(username, password, done) => {
 		User.findOne({ username: username }, (err, user) => {
 			if (err) {
 				return done(err)
@@ -22,3 +23,13 @@ const strategy = new LocalStrategy(
 )
 
 module.exports = strategy
+
+// passport.serializeUser(function(user, done) {
+// 	done(null, user.id);
+// });
+
+// passport.deserializeUser( function(id, done) {
+// 	db.Client.findByPk(id).then(function(client) {
+// 		done(null, client);
+// 	});
+// });
