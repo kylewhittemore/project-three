eb37239c8c58c0045cee87ec3e27eb5b11d29a46
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-20 18:19:55 -0400

    basic logout page structure created

---
+++ client/src/pages/LogOut/LogOut.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function LogOutPage() {
+    return(
+        <div className="">
+
+        </div>
+    );
+};
+
+export default LogOutPage;
