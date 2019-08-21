b906687978efefb1ebdb71f5b13981dcd26987e6
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-20 20:11:25 -0400

    log out confirmation screen layed out with log back in button; link to log back in and styling needed

--- client/src/pages/LogOut/LogOut.js
+++ client/src/pages/LogOut/LogOut.js
@@ -2,14 +2,22 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function LogOutPage() {
-    return(
-        <div className="">
-
-        </div>
+    return (
+        <Container>
+            <Row>
+                <Col md={6} className='log-out mx-auto'>
+                    <h3>You have been logged out!</h3>
+                    <div className='mt-5'>
+                        <Button type='submit' className='btn btn-primary btn-block'>Log In</Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
