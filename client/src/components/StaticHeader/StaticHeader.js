7fae4ce260902d0d9023849f27f781f9904f87b2
aancjr <45945137+aancjr@users.noreply.github.com>
2019-08-24 20:39:39 -0400

    initial commit, file/folder created; js file skeleton started

---
+++ client/src/components/StaticHeader/StaticHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class StaticHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isOn: true};
+    };
+
+    
+
+};
+
+export default StaticHeader;
