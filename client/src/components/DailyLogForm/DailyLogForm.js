ef1c184e2ca93bf01e952aaadceeb26e6adcd8f2
KDW <kyle.whittemore@gmail.com>
2019-08-27 11:06:05 -0400

    lined up log form, thumbnail image

--- client/src/components/DailyLogForm/DailyLogForm.js
+++ client/src/components/DailyLogForm/DailyLogForm.js
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
 import Spinner from '../LoadingSpinner/LoadingSpinner'
+import Image from 'react-bootstrap/Image'
 import Axios from 'axios'
 
 export default function DailyLog(props) {
+
+    const styles = {
+        image: {
+            height: 171 + "px",
+            // width: 80 + "px"
+            padding: 5 + "px"
+        }
+    }
 
     const initialFormState = {
         date: "",
@@ -114,89 +124,113 @@ export default function DailyLog(props) {
 
     return (
         <>
-            <Form onSubmit={handleFormSubmit} className="col-md-6">
-                <Form.Row className="m-1">
-                    <Col>
-                        <Form.Group className="m-1" controlId="log.ControlInput1">
-                            <Form.Label>Date:</Form.Label>
-                            <Form.Control value={formData.date} name="date" onChange={handleInputChange} type="date" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className="m-1" controlId="log.ControlSelect1">
-                            <Form.Label>Plant appearance:</Form.Label>
-                            <Form.Control value={formData.plantAppearance} name="plantAppearance" onChange={handleInputChange} as="select">
-                                <option>happy</option>
-                                <option>neutral</option>
-                                <option>sad</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row className="ml-2 m-1">
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox1">
-                            <Form.Check name="didWater" checked={formData.didWater} onChange={handleInputChange} type="checkbox" label="Water" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox2">
-                            <Form.Check name="didFeed" checked={formData.didFeed} onChange={handleInputChange} type="checkbox" label="Feed" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox3">
-                            <Form.Check name="didTransplant" checked={formData.didTransplant} onChange={handleInputChange} type="checkbox" label="Transplant" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-                <Form.Row>
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox4">
-                            <Form.Check name="didFlush" checked={formData.didFlush} onChange={handleInputChange} type="checkbox" label="Flush" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox5">
-                            <Form.Check name="didFlip" checked={formData.didFlip} onChange={handleInputChange} type="checkbox" label="Flip to Flower" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className="m-1" controlId="formBasicCheckbox6">
-                            <Form.Check name="didDefoliate" checked={formData.didDefoliate} onChange={handleInputChange} type="checkbox" label="Defoliate" />
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
+            <Row>
+                <Form onSubmit={handleFormSubmit} className="col-md-6">
+                    <Form.Row className="m-2">
+                        <Col>
+                            <Form.Group className="m-1" controlId="log.ControlInput1">
+                                <Form.Label>Date:</Form.Label>
+                                <Form.Control value={formData.date} name="date" onChange={handleInputChange} type="date" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="m-1" controlId="log.ControlSelect1">
+                                <Form.Label>Plant appearance:</Form.Label>
+                                <Form.Control value={formData.plantAppearance} name="plantAppearance" onChange={handleInputChange} as="select">
+                                    <option>happy</option>
+                                    <option>neutral</option>
+                                    <option>sad</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox1">
+                                <Form.Check name="didWater" checked={formData.didWater} onChange={handleInputChange} type="checkbox" label="Water" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox2">
+                                <Form.Check name="didFeed" checked={formData.didFeed} onChange={handleInputChange} type="checkbox" label="Feed" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox3">
+                                <Form.Check name="didTransplant" checked={formData.didTransplant} onChange={handleInputChange} type="checkbox" label="Transplant" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox4">
+                                <Form.Check name="didFlush" checked={formData.didFlush} onChange={handleInputChange} type="checkbox" label="Flush" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox5">
+                                <Form.Check name="didFlip" checked={formData.didFlip} onChange={handleInputChange} type="checkbox" label="Flip to Flower" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="m-1" controlId="formBasicCheckbox6">
+                                <Form.Check name="didDefoliate" checked={formData.didDefoliate} onChange={handleInputChange} type="checkbox" label="Defoliate" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Col>
+                            <Form.Group className="m-1" controlId="log.ControlTextarea1">
+                                <Form.Label>Notes:</Form.Label>
+                                <Form.Control value={formData.notes} name="notes" onChange={handleInputChange} as="textarea" rows="3" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Col>
+                            <Form.Group className="m-1" controlId="log.ControlTextarea1">
+                                {/* <Form.Label>upload:</Form.Label> */}
+                                <div className="input-group my-4">
+                                    <div className="custom-file">
+                                        <input type="file" onChange={handleUploadChange} className="custom-file-input" id="inputGroupFile02" />
+                                        <label className="custom-file-label" htmlFor="inputGroupFile02" aria-describedby="inputGroupFileAddon02">{imageName ? imageName : "Choose file"}</label>
+                                    </div>
+                                    <div className="input-group-append">
+                                        <span className="input-group-text" onClick={handleImageAttach} id="inputGroupFileAddon02">Upload</span>
+                                    </div>
+                                </div>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Col>
+                            {
+                                loadingImage ?
+                                    <Spinner />
+                                    :
+                                    <Image style={styles.image} src={imageUrl} />
+                            }
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="m-2">
+                        <Button className="m-2" variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form.Row>
+                </Form>
+            </Row>
+{/* 
+            <Row className="m-2">
                 <Col>
-                    <Form.Group className="m-1" controlId="log.ControlTextarea1">
-                        <Form.Label>Notes:</Form.Label>
-                        <Form.Control value={formData.notes} name="notes" onChange={handleInputChange} as="textarea" rows="3" />
-                    </Form.Group>
+                    {
+                        loadingImage ?
+                            <Spinner />
+                            :
+                            <Image style={styles.image} src={imageUrl} thumbnail />
+                    }
                 </Col>
-                <Col>
-                    <Form.Group className="m-1" controlId="log.ControlTextarea1">
-                        {/* <Form.Label>upload:</Form.Label> */}
-                        <div className="input-group my-4">
-                            <div className="custom-file">
-                                <input type="file" onChange={handleUploadChange} className="custom-file-input" id="inputGroupFile02" />
-                                <label className="custom-file-label" htmlFor="inputGroupFile02" aria-describedby="inputGroupFileAddon02">{imageName ? imageName : "Choose file"}</label>
-                            </div>
-                            <div className="input-group-append">
-                                 <span className="input-group-text" onClick={handleImageAttach} id="inputGroupFileAddon02">Upload</span>
-                            </div>
-                        </div>
-                    </Form.Group>
-                </Col>
-                <Button className="ml-3" variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-            {
-                loadingImage ? <Spinner />
-                    :
-                    <img src={imageUrl} />
-            }
-
+            </Row> */}
         </>
+
     )
 }
