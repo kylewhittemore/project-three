8b3cc16dfb54bfbd1fd881cfd683a040c85cbbed
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-08-31 14:05:57 -0400

    grow t

--- client/src/components/GrowTable/GrowTable.js
+++ client/src/components/GrowTable/GrowTable.js
@@ -35,10 +35,10 @@ export default function GrowTable(props) {
         })
     }, [userId]);
 
-    const goToDetails = e => {
-        e.preventDefault();
-        console.log(`clicked, growId: ${this.value}`)
-    }
+    // const goToDetails = e => {
+    //     e.preventDefault();
+    //     console.log(`clicked, growId: ${this.value}`)
+    // }
 
 
     // If the logs are loading display a spinner, otherwise render the table from state
@@ -56,7 +56,8 @@ export default function GrowTable(props) {
                             </Col>
                             <Col md="auto">
                                 <div >
-                                    <Button onClick={goToDetails()} value={grow._id}>Details</Button>
+                                    {/* <Button onClick={goToDetails()} value={grow._id}>Details</Button> */}
+                                    <Button >Details</Button>
                                     <Button value={grow._id}>Make Default</Button>
                                 </div>
                             </Col>
