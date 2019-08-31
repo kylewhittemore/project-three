0039af51660e2e257ffcaf2e31170c14065f586c
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-08-31 01:10:27 -0400

    seasons page with individual season building blocks

--- client/src/components/GrowTable/GrowTable.js
+++ client/src/components/GrowTable/GrowTable.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Figure from 'react-bootstrap/Figure'
 import Axios from 'axios'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
+import './style.css'
 
 export default function GrowTable(props) {
+
+    // const leafLink = '../../../public/leaf.png'
     const userId = localStorage.getItem('p3aajjkw-id')
-    // const styles = {
-    //     icon: {
-    //         cursor: "pointer"
-    //     },
-    //     placeholder: {
-    //         visibility: "hidden"
-    //     }
-    // }
 
     const [grows, setGrows] = useState([])
     const [loading, setLoading] = useState(true)
@@ -22,11 +22,12 @@ export default function GrowTable(props) {
             setLoading(true);
             let response = await Axios.get(`/api/grow/user/${userId}`);
             let data = response.data
-            console.log(data)
+            console.log(JSON.stringify(data))
             return data;
         }
         fetchGrows().then(data => {
             setGrows(data)
+            // console.log(JSON.stringify)
             setLoading(false)
         }).catch(err => {
             console.log(err)
@@ -34,57 +35,65 @@ export default function GrowTable(props) {
         })
     }, [userId]);
 
-    // async function getGrows() {
-    //     let response = await Axios.get('/api/grow')
-    //     setGrows(response.data)
+    // function goToDetails(growId) {
+    //     return (
+    //         <StaticHeader {...props} growId={growId} />
+    //     )
     // }
 
-    // async function deleteGrow(id) {
-    //     let response = await Axios({
-    //         method: 'delete',
-    //         url: `/api/grow/${id}`
-    //     })
-    //     return response
-    //     //needs logic to delete all associated logs
-    // }
-
-    function TableHead() {
-        return (
-            <thead>
-                <tr>
-                    <th>Season Name</th>
-                    <th>Strain Name</th>
-                    <th>Data Started</th>
-                    <th>Date Completed</th>
-                </tr>
-            </thead>
-        )
-    }
-
-    function TableBody() {
-        return (
-            <tbody>
-                {grows.map(grow => (
-                    <tr key={grow._id}>
-                        <td>{grow.seasonName}</td>
-                        <td>{grow.strainName}</td>
-                        <td>{grow.dateStarted ? grow.dateStarted.slice(0, 10) : "Not Started"}</td>
-                        <td>{!grow.dateCompleted && !grow.dateStarted ? '' : grow.dateCompleted ? grow.dateCompleted.slice(0, 10) : "In Progress"}</td>
-                    </tr>
-                ))}
-            </tbody>
-        )
-    }
 
     // If the logs are loading display a spinner, otherwise render the table from state
     return (
         loading ?
             <LoadingSpinner />
             :
-            <Table>
-                <TableHead />
-                <TableBody />
-            </Table>
+            <Container>
+                   {grows.map(grow => (
+                    <div key={grow._id}>
+                        <Row >
+                            <Col >
+                                <h2 className="font-italic">{grow.seasonName}</h2>
+                                <p >System Id: {grow._id}</p>
+                            </Col>
+                            <Col md="auto">
+                                <div >
+                                    <Button onClick >Details</Button>
+                                    <Button >Make Default</Button>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col md={4}>
+                                <Figure>
+                                    <Figure.Image
+                                        width={152}
+                                        height={152}
+                                        alt="171x180"
+                                        src="./leaf.png"
+                                    />
+                                    <Figure.Caption>
+                                        Nulla vitae elit libero, a pharetra augue mollis interdum.
+                                </Figure.Caption>
+                                </Figure>
+                            </Col>
+                            <Col md={8}>
+                                <ListGroup>
+                                    <ListGroup.Item>Strain Name: {grow.strainName}</ListGroup.Item>
+                                    <ListGroup.Item>Breeder: {grow.breeder}</ListGroup.Item>
+                                    <ListGroup.Item>Start Date: {grow.dateStarted
+                                        ? grow.dateStarted.slice(0, 10)
+                                        : "Not Started"} </ListGroup.Item>
+                                    <ListGroup.Item>Date Completed: {!grow.dateCompleted && !grow.dateStarted
+                                        ? ''
+                                        : grow.dateCompleted
+                                            ? grow.dateCompleted.slice(0, 10)
+                                            : "In Progress"} </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </div>
+                ))}
+            </Container>
     )
 
 }
