cd4795c4ec9eb0a309811fab16f09349fcdf9e3e
KDW <kyle.whittemore@gmail.com>
2019-09-02 10:24:46 -0400

    stash

--- client/src/pages/DailyLog/DailyLog.js
+++ client/src/pages/DailyLog/DailyLog.js
@@ -1,39 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DailyLogForm from '../../components/DailyLogForm/DailyLogForm'
 import LeftSliderBar from "../../components/LeftSliderBar/LeftSliderBar";
 import { Redirect } from 'react-router-dom'
+import Axios from 'axios'
 
 function DailyLogPage(props) {
+    const [user, setUser] = useState('')
+    const [growId, setGrowId] = useState('')
+    const [logId, setLogId] = useState('')
+    const [loading, setLoading] = useState(false)
 
-    const userId = localStorage.getItem('p3aajjkw-id')
+    // const userId = localStorage.getItem('p3aajjkw-id')
 
-    if (!userId) {
-        return (
-            <Redirect to={'/'} />
-        )
-    }
+    // if (!userId) {
+    //     return (
+    //         <Redirect to={'/'} />
+    //     )
+    // }
 
-    let logId, growId
 
-    let url = window.location.href
+    useEffect(() => {
+        async function fetchUser() {
+            setLoading(true);
+            let response = await Axios.get(`/api/user/profile`);
+            let data = response.data
+            console.log("USER: " + JSON.stringify(data))
+            setLoading(false)
+            return data.user;
+        }
+        fetchUser().then(user => {
+            if (!user._id) {
+                return (
+                    <Redirect to={'/'} />
+                )
+            }
+            console.log("fetch user", user)
+            setUser(user)
+            return user
+        })
+            .then(user => {
 
-    if (url.indexOf("?log_id=") !== -1) {
-        logId = url.split("=")[1]
-    }
-    else if (url.indexOf("?grow_id=") !== -1) {
-        growId = url.split("=")[1]
-    }
+                let url = window.location.href
+
+                if (url.indexOf("?log_id=") !== -1) {
+                    setLogId(url.split("=")[1])
+                }
+                else if (url.indexOf("?grow_id=") !== -1) {
+                    setGrowId(url.split("=")[1])
+                } else {
+                    setGrowId(user.defaultGrow)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, []);
+
+    // let url = window.location.href
+
+    // if (url.indexOf("?log_id=") !== -1) {
+    //     setLogId(url.split("=")[1])
+    // }
+    // else if (url.indexOf("?grow_id=") !== -1) {
+    //     setGrowId(url.split("=")[1])
+    // } else {
+    //     setGrowId(user.defaultGrow)
+    // }
 
     return (
         <>
             <LeftSliderBar {...props} />
             <div className="align-me">
                 <DailyLogForm {...props}
-                    // growId={growId}
+                    growId={growId}
                     logId={logId}
-                    userId={userId}
+                    userId={user._id}
                 />
             </div>
         </>
