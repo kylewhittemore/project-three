3cca8420101183e0d3ce871377d63e874495394f
KDW <kyle.whittemore@gmail.com>
2019-09-04 00:07:46 -0400

    renderinfg last events

---
+++ client/src/components/Dashboard/dashboard-kw.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { Alert, Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
+
+export default function Dashboard(props) {
+
+    const [grow, setGrow] = useState({})
+    const [waterHistory, setWaterHistory] = useState([])
+    const [feedHistory, setFeedHistory] = useState([])
+
+    useEffect(() => {
+
+        async function fetchGrow(growId) {
+            let response = await Axios.get(`/api/grow/${growId}`)
+            return response
+        }
+
+        async function fetchUser() {
+            let data = await Axios.get(`/api/user/profile`)
+            let user = data.data.user
+            return user
+        }
+
+        if (props.growId) {
+            fetchGrow(props.growId)
+                .then(response => console.log(response))
+        } else {
+        fetchUser()
+            .then(user => {
+                console.log("USER:  ", user)
+                return user.defaultGrow
+            })
+            .then(async growId => {
+                let response = await fetchGrow(growId)
+                setGrow(response.data)
+                console.log(response.data)
+                setWaterHistory(response.data.dailyLogs.filter(log => log.didWater))
+                setFeedHistory(response.data.dailyLogs.filter(log => log.didFeed))
+
+                return response
+            })
+        }
+    }, [])
+
+    function ListData() {
+        return (
+                <ListGroup>
+                    <ListGroup.Item><strong>Strain: </strong>{grow.strainName}</ListGroup.Item>
+                    
+                    <ListGroup.Item><strong>Last Date Watered: </strong>{waterHistory.map((element, index) => index === 0 ? element.date : "" )}</ListGroup.Item>
+                    <ListGroup.Item><strong>Last Data Fed: </strong>{feedHistory.map((element, index) => index === 0 ? element.date : "" )}</ListGroup.Item>
+                </ListGroup>
+        )
+    }
+
+    return (
+
+        <Container>
+            <Row>
+                <Col className="text-right">
+                    <Button onClick={event => {
+                        event.preventDefault()
+                        props.history.push(`/newseason/?grow_id=${props.growId}`)
+                    }} variant="outline-dark" size="sm">Edit</Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h2 className="text-center">{grow.seasonName}</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6} className="text-center">
+                    <p><strong>Date Strated: </strong>{grow.dateStarted}}</p>
+                </Col>
+                <Col md={6} className="text-center">
+                    <p><strong>Date Completed: </strong>{grow.dateCompleted}</p>
+                </Col>
+            </Row>
+
+            <Row>
+                <ListData />
+            </Row>
+        </Container>
+
+    )
+}
