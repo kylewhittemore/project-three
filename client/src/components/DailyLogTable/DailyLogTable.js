18ed4858b34a34aa8657af1edc6c8a62156b0dd5
KDW <kyle.whittemore@gmail.com>
2019-09-05 23:40:01 -0400

    DLT shows only logs from the grow in the url

--- client/src/components/DailyLogTable/DailyLogTable.js
+++ client/src/components/DailyLogTable/DailyLogTable.js
@@ -16,20 +16,28 @@ export default function DailyLogTable(props) {
     }
 
     const [logs, setLogs] = useState([])
-    const [loading, setLoading] = useState(true)    
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function fetchLogs() {
             setLoading(true);
-            let response = await Axios.get('/api/daily'); // userId
+
+            let response = await Axios.get(`/api/daily/grow/${props.growId}`); // userId
+            console.log("logs", response)
             let data = response.data
             return data;
         }
         fetchLogs().then(data => {
+            console.log(props.growId)
+            // let filtered
+            // let filtered = data.filter(element => element.grow === props.growId)
+            // let filtered = data.filter(element => element)
+            // console.log("filtered", data)
             setLogs(data)
+            // setLogs(data)
             setLoading(false)
         })
-        .catch(err => setLoading(false))
+            .catch(err => setLoading(false))
     }, []);
 
     async function getLogs() {
@@ -65,41 +73,42 @@ export default function DailyLogTable(props) {
 
     function TableBody() {
         return (
+
             <tbody>
-                {/* after implementing filters "logs.map....""  will be "filteredLogs.map..." */}
                 {logs.map(log => {
 
                     // console.log(log.grow.seasonName)
                     return (
-                    <tr key={log._id}>
-                        <td>{log.logId}</td>
-                        <td>{log.date.slice(0, 10)}</td>
-                        <td></td>
-                        {/* <td>{log.grow.seasonName}</td> */}
-                        <td>
-                            {log.notes ? <i style={styles.icon} className="p-1 far fa-sticky-note" onClick={event => {
-                                event.preventDefault()
-                                getNotes(log._id).then(notes => console.log(notes))
+                        <tr key={log._id}>
+                            <td>{log.logId}</td>
+                            <td>{log.date.slice(0, 10)}</td>
+                            <td></td>
+                            {/* <td>{log.grow.seasonName}</td> */}
+                            <td>
+                                {log.notes ? <i style={styles.icon} className="p-1 far fa-sticky-note" onClick={event => {
+                                    event.preventDefault()
+                                    getNotes(log._id).then(notes => console.log(notes))
 
-                            }}
-                            ></i>
-                            :
-                            <i style={styles.placeholder} className="p-1 far fa-sticky-note"></i>
-                            }
-                            <i style={styles.icon} className="p-1 far fa-edit" onClick={event => {
-                                event.preventDefault()
-                                // updateLog(log._id).then(getLogs)
-                                props.history.push(`/dailylog/?log_id=${log._id}`)
-                            }}
-                            ></i>
-                            <i style={styles.icon} className="p-1 far fa-trash-alt" onClick={event => {
-                                event.preventDefault()
-                                deleteLog(log._id).then(getLogs)
-                            }}
-                            ></i>
-                        </td>
-                    </tr>
-                  )})}
+                                }}
+                                ></i>
+                                    :
+                                    <i style={styles.placeholder} className="p-1 far fa-sticky-note"></i>
+                                }
+                                <i style={styles.icon} className="p-1 far fa-edit" onClick={event => {
+                                    event.preventDefault()
+                                    // updateLog(log._id).then(getLogs)
+                                    props.history.push(`/dailylog/?log_id=${log._id}`)
+                                }}
+                                ></i>
+                                <i style={styles.icon} className="p-1 far fa-trash-alt" onClick={event => {
+                                    event.preventDefault()
+                                    deleteLog(log._id).then(getLogs)
+                                }}
+                                ></i>
+                            </td>
+                        </tr>
+                    )
+                })}
             </tbody>
         )
     }
