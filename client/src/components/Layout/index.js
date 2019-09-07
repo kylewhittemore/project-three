4b2b5ed3439cc66f2ec4101fbc84a78b81ba1f98
KDW <kyle.whittemore@gmail.com>
2019-09-07 11:57:40 -0400

    routing without the slider on login/logout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -5,3 +5,5 @@ export { default as Header } from './Header';
 // export { default as LayoutRoute } from './LayoutRoute';
 export { default as MainLayout } from './MainLayout';
 export { default as LeftSliderBar } from '../LeftSliderBar/LeftSliderBar';
+export { default as EmptyLayout } from './EmptyLayout'
+export { default as LayoutRoute } from './LayoutRoute'
