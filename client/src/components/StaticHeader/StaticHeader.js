bfb1387a65ef85ebacbf11fe237bef0540b70144
aancjr <45945137+aancjr@users.noreply.github.com>
2019-09-08 18:45:05 -0400

    header is formatted well. May need to set breakpoints, but looks better

--- client/src/components/StaticHeader/StaticHeader.js
+++ client/src/components/StaticHeader/StaticHeader.js
@@ -39,40 +39,6 @@ class StaticHeader extends Component {
         // redirect: false
     };
 
-    // async editForm(event) {
-    //     event.preventDefault();
-    //     console.log("hi!")
-    //     if (this.props.growId) {
-    //         console.log("grow id:" , this.props.growId);
-    //         let response = await Axios.get(`/api/grow/${this.props.growId}`);
-    //         console.log(response);
-    //         let data = response.data;
-    //         this.setState({
-    //             seasonName: data.seasonName,
-    //             dateStarted: data.dateStarted.slice(0, 10),
-    //             dateCompleted: data.dateCompleted.slice(0, 10),
-    //             strainName: data.strainName,
-    //             lineage: data.lineage,
-    //             floweringTime: data.floweringTime,
-    //             breeder: data.breeder,
-    //             starterPlantType: data.starterPlantType,
-    //             numPlants: data.numPlants,
-    //             medium: data.medium,
-    //             vegLightType: data.vegLightType,
-    //             vegLightWattage: data.vegLightWattage,
-    //             flowerLightType: data.flowerLightType,
-    //             flowerLightWattage: data.flowerLightWattage,
-    //             lightNotes: data.lightNotes,
-    //             canopyTechnique: data.canopyTechnique,
-    //             canopyTechniqueNotes: data.canopyTechniqueNotes
-    //         });
-    //         console.log("router", this.props.router)
-    //         await this.props.history.push(`grow/${this.props.growId}`)
-    //     };
-    // };
-
-
-
     async componentDidMount() {
         this.setState({ loading: true })
         let response = await Axios.get(`/api/grow/${this.props.growId}`);
@@ -102,7 +68,7 @@ class StaticHeader extends Component {
         console.log("DATA", data.coverImage)
     };
 
-    
+
     render() {
 
         function formatMonDateShort(date) {
@@ -115,75 +81,75 @@ class StaticHeader extends Component {
                     :
                     <Container className="staticHeaderContainer mx-auto">
                         <Row>
-                            <Col md="8" className="mx-auto text-left">
-                                <h2 className="text-capitalize">{this.state.seasonName}</h2>
-                            </Col>
-                            <Col className="text-right">
-                                <Button className="text-right btn-info"
-                                    onClick={event => {
-                                        event.preventDefault()
-                                        console.log("history", this.props.history)
-                                        this.props.history.push(`/newseason/?grow_id=${this.props.growId}`)
-                                    }} variant="outline-light" size="sm">Edit</Button>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="2" className="my-auto">
+                            <Col xs="3" className="my-auto p-0 text-center">
                                 <Figure className="my-auto staticImage">
                                     <Figure.Image
                                         // width={171}
                                         // height={180}
-                                        className="img-fluid img-thumbnail"
+                                        className="img-fluid img-thumbnail m-0"
                                         alt="171x180"
                                         src={`https://project-three-logger-photos.s3.amazonaws.com/${this.state.coverImage}`}
                                     >
                                     </Figure.Image>
                                 </Figure>
                             </Col>
-                            {/* <Row> */}
+
+                            <Col xs="9">
+                                <Row>
+                                    <Col xs="10" className="mx-auto text-left">
+                                        <h2 className="text-capitalize">{this.state.seasonName}</h2>
+                                    </Col>
+                                    <Col xs="2" className="text-right">
+                                        <Button className="text-right btn-info"
+                                            onClick={event => {
+                                                event.preventDefault()
+                                                console.log("history", this.props.history)
+                                                this.props.history.push(`/newseason/?grow_id=${this.props.growId}`)
+                                            }} variant="outline-light" size="sm">Edit</Button>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col md="6" className="my-auto">
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Started: </strong>{formatMonDateShort(this.state.dateStarted)}</p>
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Harvest: </strong>{formatMonDateShort(this.state.dateCompleted)}</p>
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Strain: </strong>{this.state.strainName}</p>
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Flowering Time: </strong>{`${this.state.floweringTime} Days`}</p>
+                                    </Col>
+
+                                    <Col md="6" className="my-auto">
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Lineage: </strong>{this.state.lineage}</p>
+                                        <p className="my-1 headerInfo ellipses"><strong className="headerInfoTitle">Breeder: </strong>{this.state.breeder}</p>
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Canopy Technique: </strong>{this.state.canopyTechnique}</p>
+                                        <p className="my-1 headerInfo"><strong className="headerInfoTitle">Starter Plant Type: </strong>{`${this.state.numPlants} ${this.state.starterPlantType} in ${this.state.medium}`}</p>
+                                    </Col>
+                                </Row>
+
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col md="3" className="mx-auto my-auto">
+                                <h5 className="text-center mb-0 pb-0">Environment and Lighting Details</h5>
+                            </Col>
+
                             <Col md="3" className="my-auto">
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Started: </strong>{formatMonDateShort(this.state.dateStarted)}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Harvest: </strong>{formatMonDateShort(this.state.dateCompleted)}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Flowering Time: </strong>{this.state.floweringTime} Days</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Medium: </strong>{this.state.medium}</p>
-
+                                <p className="my-1 headerInfo"><strong className="headerInfoTitle">Veg Light: </strong>{`${this.state.vegLightWattage} Watt ${this.state.vegLightType}`}</p>
+                                <p className="my-1 headerInfo"><strong className="headerInfoTitle">Flower Light: </strong>{`${this.state.flowerLightWattage} Watt ${this.state.flowerLightType}`}</p>
                             </Col>
-                            <Col md="6" className="my-auto">
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Strain: </strong>{this.state.strainName}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Lineage: </strong>{this.state.lineage}</p>
-                                <p className="my-1 dashInfo ellipses"><strong className="dashInfoTitle">Breeder: </strong>{this.state.breeder}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Starter Plant Type: </strong>{this.state.numPlants} {this.state.starterPlantType}</p>
 
+                            <Col md="3" className="my-auto">
+                                <p className="my-1 text-center headerInfo"><strong className="headerInfoTitle">Lighting Notes</strong></p>
+                                <p>{this.state.lightNotes}</p>
                             </Col>
-                            {/* <Col xs="3">
-                                
 
-                            </Col> */}
+                            <Col md="3" className="my-auto">
+                                <p className="my-1 text-center headerInfo"><strong className="headerInfoTitle">Canopy Notes</strong></p>
+                                <p>{this.state.canopyTechniqueNotes}</p>
+                            </Col>
+
                         </Row>
-                        <Row>
-                            <h5 className="mx-auto">Environment and Lighting Details</h5>
-                        </Row>
-                        <Row>
-                            <Col md="4">
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Veg Light: </strong>{this.state.vegLightWattage} Watt {this.state.vegLightType}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Flower Light: </strong>{this.state.flowerLightWattage} Watt {this.state.flowerLightType}</p>
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Canopy Technique: </strong>{this.state.canopyTechnique}</p>
 
-                                {/* <p className="my-1 dashInfo"><strong className="dashInfoTitle">Veg Light Wattage: </strong></p> */}
-                            </Col>
-                            <Col md="4">
-                            <p className="my-1 dashInfo"><strong className="dashInfoTitle">Lighting Notes: </strong>{this.state.lightNotes}</p>
-
-                                {/* <p className="my-1 dashInfo"><strong className="dashInfoTitle">Flower Light Wattage: </strong></p> */}
-                            </Col>
-                            <Col md="4">
-
-                                <p className="my-1 dashInfo"><strong className="dashInfoTitle">Canopy Technique Notes: </strong>{this.state.canopyTechniqueNotes}</p>
-                            </Col>
-                            {/* <Col xs="3">
-
-                            </Col> */}
-                        </Row>
                     </Container>
                 }
             </div>
